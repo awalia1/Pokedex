c69f5132540652325a9c4b8b8765e68a98af9b63
Amarpreet Walia <amarpreet.s.walia@gmail.com>
2018-07-17 17:31:19 -0400

    used sample ajax

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-class Pokemon
+// function loadDoc() {
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//       document.getElementById("info").innerHTML = this.responseText;
+//     }
+//   };
+//   xhttp.open("GET", "https://raw.githubusercontent.com/mlaw-nycda/data/master/data", true);
+//   xhttp.send();
+// }
+
+trainer = {
+	squirtle : {
+		name : 
+	}
+}
