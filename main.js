1eca314dcc299352cb34991751f686ea908a3e9b
Amarpreet Walia <amarpreet.s.walia@gmail.com>
2018-07-18 14:12:31 -0400

    got done the correct calls

--- main.js
+++ main.js
@@ -9,8 +9,45 @@
 //   xhttp.send();
 // }
 
-trainer = {
-	squirtle : {
-		name : 
+class Pokemon {
+	constructor(){
+		this.name = myobj.name;
+		this.defense = myObj.stats[3].base_stat;
+		this.attack = myObj.stats[4].base_stat;
+		this.hp = myObj.stats[5].base_stat;
+		this.abilities = myObj.abilities[i].ability.name;
 	}
 }
+
+class squirtle extends Pokemon {
+	constructor(){
+		super();
+		this.id = "7";
+	}
+}
+
+class squirtle extends Pokemon {
+	constructor(){
+		super();
+		this.id = "6";
+	}
+}
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        console.log(Pokemon);
+        for(i=0;i<myObj.abilities.length;i++){
+        	var h = document.createElement("H1");
+    var t = document.createTextNode(myObj.abilities[i].ability.name);
+    h.appendChild(t);
+    document.body.appendChild(h);
+        	// console.log(myObj.abilities[i].ability.name);
+        // document.getElementById("demo").innerHTML = myObj.abilities[i].ability.name;
+        
+    }
+    }
+};
+xmlhttp.open("GET", "https://raw.githubusercontent.com/awalia1/Pokedex/master/squirtle.txt", true);
+xmlhttp.send();
