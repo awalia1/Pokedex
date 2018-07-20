f66230fb0877cc444e749d47d59182562e42b217
Amarpreet Walia <amarpreet.s.walia@gmail.com>
2018-07-20 11:32:23 -0400

    figured out JSON parse on click

--- main.js
+++ main.js
@@ -9,6 +9,8 @@
 //   xhttp.send();
 // }
 
+
+//possible graph that doesnt work
 // google.load('visualization', '1', {packages: ['corechart']});
 // google.setOnLoadCallback(drawMaterial);
 
@@ -36,40 +38,58 @@ function squirtleClick() {
 	var a = document.getElementById('pokemon');
 	var b = document.getElementById("description");
 	var c = document.getElementById("stats");
-	var c1 = document.getElementById("hp");
-	var c2 = document.getElementById("atk");
-	var c3 = document.getElementById("def");
+
+	b.classList.add("box");
+	c.classList.add("box");
 
 	document.body.style.backgroundImage = 'url(images/water.jpg)';
 
 	document.getElementById("name").innerHTML = "Squirtle";
 
-	var img = document.createElement('img');
-    img.src = "https://pre00.deviantart.net/391f/th/pre/i/2015/331/1/d/squirtle_with_a_default_happy_face_by_kol98-d9ia5zu.png";
-    a.appendChild(img);
+   	document.getElementById("big-pokemon").src = 'images/squirtle-big.jpg';
 
     b.innerHTML = "Squirtle's shell is not merely used for protection. The shell's rounded shape and the grooves on its surface help minimize resistance in water, enabling this Pokemon to swim at high speeds.";
     var ability = document.createElement('p');
-    ability.innerHTML = "This Pokemon's abiltiies include water and not."
+    ability.innerHTML = "This Pokemon's abilties include water and not."
     b.appendChild(ability);
 
-    var hp = document.createElement('p');
-    var atk = document.createElement('p');
-    var def = document.createElement('p');
-    var stat = document.createElement('h2');
+    var hp = document.getElementById('hp');
+    var atk = document.getElementById('atk');
+    var def = document.getElementById('def');
+
+    hp.innerHTML= "HP: 90";
+    atk.innerHTML= "ATK: 90";
+    def.innerHTML= "DEF: 90";
+ 
+}
+
+function venusaurClick() {
+	var a = document.getElementById('pokemon');
+	var b = document.getElementById("description");
+	var c = document.getElementById("stats");
+
+	b.classList.add("box");
+	c.classList.add("box");
+
+	document.body.style.backgroundImage = 'url(images/grass.jpg)';
+
+	document.getElementById("name").innerHTML = "Venusaur";
+
+    document.getElementById("big-pokemon").src = 'images/venusaur-big.jpg';
+
+    b.innerHTML = "Squirtle's shell is not merely used for protection. The shell's rounded shape and the grooves on its surface help minimize resistance in water, enabling this Pokemon to swim at high speeds.";
+    var ability = document.createElement('p');
+    ability.innerHTML = "This Pokemon's abilties include nine and ten."
+    b.appendChild(ability);
+
+    var hp = document.getElementById('hp');
+    var atk = document.getElementById('atk');
+    var def = document.getElementById('def');
 
     hp.innerHTML= "HP: 90";
     atk.innerHTML= "ATK: 90";
     def.innerHTML= "DEF: 90";
 
-    c.appendChild(hp);
-    c.appendChild(atk);
-    c.appendChild(def);
-    
-}
-
-function venusaurClick() {
-	document.body.style.backgroundImage = 'url(images/grass.jpg)';
 }
 
 function pikachuClick() {
@@ -86,11 +106,12 @@ function charizardClick() {
 
 class Pokemon {
 	constructor(){
-		this.name = myobj.name;
-		this.defense = myObj.stats[3].base_stat;
-		this.attack = myObj.stats[4].base_stat;
-		this.hp = myObj.stats[5].base_stat;
-		this.abilities = myObj.abilities[i].ability.name;
+		this.name = "myobj.name";
+		this.defense = "myObj.stats[3].base_stat";
+		this.attack = "myObj.stats[4].base_stat";
+		// this.hp = myObj.stats[5].base_stat;
+		// this.ability1 = myObj.abilities[0].ability.name;
+		// this.ability2 = myObj.abilities[1].ability.name;
 	}
 }
 
@@ -98,6 +119,9 @@ class Squirtle extends Pokemon {
 	constructor(){
 		super();
 		this.id = "7";
+		this.imgSrc = "images/squirtle-big.jpg";
+		this.bgImg = "images/water.jpg";
+		this.description = "Squirtle's shell is not merely used for protection. The shell's rounded shape and the grooves on its surface help minimize resistance in water, enabling this Pokemon to swim at high speeds.";
 		update(this);
 	}
 }
@@ -105,7 +129,10 @@ class Squirtle extends Pokemon {
 class Venasaur extends Pokemon {
 	constructor(){
 		super();
-		this.id = "6";
+		this.id = "3";
+		this.imgSrc = "images/venasaur-big.jpg";
+		this.bgImg = "images/grass.jpg";
+		this.description = "There is a large flower on Venusaur's back. The flower is said to take on vivid colors if it gets plenty of nutrition and sunlight. The flower's aroma soothes the emotions of people.";
 		update(this);
 	}
 }
@@ -113,7 +140,10 @@ class Venasaur extends Pokemon {
 class Pikachu extends Pokemon {
 	constructor(){
 		super();
-		this.id = "6";
+		this.id = "25";
+		this.imgSrc = "images/pikachu-big.jpg";
+		this.bgImg = "images/electric.jpg";
+		this.description = "Whenever Pikachu comes across something new, it blasts it with a jolt of electricity. If you come across a blackened berry, it's evidence that this Pokémon mistook the intensity of its charge."
 		update(this);
 	}
 }
@@ -121,7 +151,10 @@ class Pikachu extends Pokemon {
 class Nidoking extends Pokemon {
 	constructor(){
 		super();
-		this.id = "6";
+		this.id = "34";
+		this.imgSrc = "images/nidoking-big.jpg";
+		this.bgImg = "images/poison.jpg";
+		this.description = "Nidoking's thick tail packs enormously destructive power. With one swing, it can topple a metal transmission tower. Once this Pokémon goes on a rampage, there is no stopping it."
 		update(this);
 	}
 }
@@ -130,9 +163,13 @@ class Charizard extends Pokemon {
 	constructor(){
 		super();
 		this.id = "6";
+		this.imgSrc = "images/charizard-big.jpg";
+		this.bgImg = "images/fire.jpg";
+		this.description = "Charizard flies around the sky in search of powerful opponents. It breathes fire of such great heat that it melts anything. However, it never turns its fiery breath on any opponent weaker than itself."
 		update(this);
 	}
 }
+
 function update(char) {
 
 	var title = document.createElement('h1');
